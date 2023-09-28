4b9eda6559c59775ff144e422fd02e0e09796435
raghadsalameh1 <90514155+raghadsalameh1@users.noreply.github.com>
2023-09-28 12:53:30 +0000

    remove minio stuff from settings (#39)

--- src/settings/config.py
+++ src/settings/config.py
@@ -2,10 +2,6 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    MINIO_SERVER: str
-    MINIO_ACCESS_KEY: str
-    MINIO_SECRET_KEY: str
-    MINIO_BUCKET_NAME: str
     DB_HOST: str
     DB_USER: str
     DB_PASSWORD: str
